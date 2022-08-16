7ae21e7dfb1d142c2a2e7546b0326dbdb167db88
Thummakeerthi <110010517+Thummakeerthi@users.noreply.github.com>
2022-08-16 21:05:03 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,4 +13,4 @@ while len(actual_list) < length_of_circular_linked_list and value < len(circular
     value += 1
 
 print(len(actual_list))
-print(" ".join(str(num) for num in actual_list
+print(" ".join(str(num) for num in actual_list))
